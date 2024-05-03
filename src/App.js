864da0d1bc6174fb167565ac6978c042d0aa06f0
sumedhnbarsagade <sbarsagade248@gmail.com>
2024-05-03 15:04:43 +0530

    shopify zone project setup

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Navigation from './customer/components/Navigation/Navigation';
+
+function App() {
+  return (
+    <div className=''>
+      <Navigation/>
+      <div>
+        Home
+      </div>
+    </div>
+  );
+}
+
+export default App;
